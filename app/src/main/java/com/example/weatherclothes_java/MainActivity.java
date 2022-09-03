7b500d2afdee1ac01114731b19ddee7cd464bc27
Yuhee Kim <dbgml4509@naver.com>
2022-09-03 10:55:28 +0900

    fix : build gradle 오류 수정

--- app/src/main/java/com/example/weatherclothes_java/MainActivity.java
+++ app/src/main/java/com/example/weatherclothes_java/MainActivity.java
@@ -2,21 +2,34 @@ package com.example.weatherclothes_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.example.weatherclothes_java.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch(event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                Intent intent = new Intent(this, WeathermenuView.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                finish();
+                break;
+        }
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
-
-
 
 
 
